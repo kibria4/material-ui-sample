895b85df2aa1e4f35b40057d9226f5f24c0287be
Kibria Ali <kibria4@live.co.uk>
2019-02-07 21:09:36 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -2,27 +2,116 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+//Modal-specific
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+/**
+ * Modal part
+ */
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}//END modal part
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  paper: { //modal part
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+  }, //end modal part
+});
+
 class App extends Component {
+
+  state = {
+    value: 0,
+    open: false, //modal part
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+  /** Modal part  */
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  /** END modal part */
+
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange} centered>
+            <Tab label="Login" />
+            <Tab label="Register" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer>
+          <Button onClick={this.handleOpen}>Open Modal</Button>
+          <Modal
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            open={this.state.open}
+            onClose={this.handleClose}
           >
-            Learn React
-          </a>
-        </header>
+            <div style={getModalStyle()} className={classes.paper}>
+              <Typography variant="h6" id="modal-title">
+                Text in a modal
+              </Typography>
+              <Typography variant="subtitle1" id="simple-modal-description">
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </div>
+          </Modal>
+        </TabContainer>}
+        {value === 1 && <TabContainer>Add Register form here</TabContainer>}
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
